6bf4fe9fe137517b39ce56a5d959fac080b27b90
huangfeng <huangfeng@163.com>
2017-07-26 12:06:03 +0800

    05

--- swiftlearn/ViewController.swift
+++ swiftlearn/ViewController.swift
@@ -15,13 +15,18 @@ class ViewController: UIViewController {
         
         demo02()
         
+        demoblock()
+        
+        demo03()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
     func demo02() {
-        var usernames = ["Lves","wildcat","Cc","Lecoding","Cd"]
+        let usernames = ["Lves","wildcat","Cc","Lecoding","Cd"]
         
-        var resultname = usernames.sorted(by:{s1,s2 in
+        let resultname = usernames.sorted(by:{s1,s2 in
             return s1<s2
         })
         
@@ -31,17 +36,83 @@ class ViewController: UIViewController {
     
     
     func demoblock() {
-        var usernames = ["Lves","wildcat","Cc","Lecoding","Cd"]
+        let usernames = ["Lves","wildcat","Cc","Lecoding","Cd"]
 //        var resultName1 = usernames.sorted(by: backWords(s1:s2:))
-        var resultName1 = usernames.sorted()
+        let resultName1 = usernames.sorted()
         
         print(resultName1)
     }
 
     
-
     func backWords(s1:String,s2:String) -> Bool {
         return s1>s2
+    }
+    
+    func demo03()  {
+        var myVariable = 42
+        myVariable = 50
+        let myConstant = 42
+        print(myVariable,myConstant)
+        
+        
+        let explicitDouble:Double = 70
+        let label = "The width is"
+        let widthLabel = label+" "+String(explicitDouble)
+        print(widthLabel)
+        
+        let fruitSummary = "I have \(myVariable) pieces of fruit"
+        print(fruitSummary)
+        
+        
+        var shoppingList = ["catfish","water","tulips","blue paint"]
+        shoppingList[1] = "bottle of water"
+        
+        var occupations = [
+            "Malcolm": "Captain",
+               "Kaylee":"Mechanic",
+        ]
+        
+        occupations["Jayne"] = "Public Relations"
+        print(shoppingList,occupations)
+        
+        var emptyArray = [String]()
+        _ = [String:Float]()
+        emptyArray.append("one")
+        print(emptyArray)
+        
+        for str in emptyArray {
+            if str == "one" {
+                print(str+"相等")
+            }
+            
+            if str < "ones" {
+                print(str+"< ones")
+            }
+            
+            if str.hasPrefix("on") {
+                print(str+" hasprefix (on)")
+            }
+            
+            if str.contains("n") {
+                print(str+" contains (n)")
+            }
+            
+            if str.hasSuffix("ne") {
+                print(str+" 以 (ne)结尾")
+            }
+        }
+        
+        
+        var optionalString:String? = "hello"
+        print(optionalString == nil)
+        
+        var optionName:String? = "John Appleseed"
+        var greeting = "hello!"
+        if let name = optionName {
+            greeting = "Hello, \(name)"
+        }
+        
+        
     }
     
 }
