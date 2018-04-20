817c680bd22c016ba9f7fb285ac009770abde1d0
huangfeng <huangfeng@163.com>
2018-04-20 17:42:33 +0800

    swift4.1

---
+++ swift4.1_Playground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+var str = "Hello, playground"
+print("\(str)")
+
+
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+let a = Int.min
+let b = Int.max
+let c = UInt.min
+let d = UInt.max
+
+let meaningOfLife = 42
+type(of: meaningOfLife)
+let pi = 3.1416
+type(of: pi)
+let anotherPi = 3 + 0.111
+
+let decimlInteger = 17
+let binaryInteger = 0b1001
+let octalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+
+
+
