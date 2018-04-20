82d98768f2cca80a5ea5a8c8722ca0b0916ea934
huangfeng <huangfeng@163.com>
2018-04-20 18:30:03 +0800

    ...

--- swift4.1_Playground.playground/Contents.swift
+++ swift4.1_Playground.playground/Contents.swift
@@ -25,6 +25,16 @@ let decimlInteger = 17
 let binaryInteger = 0b1001
 let octalInteger = 0o21
 let hexadecimalInteger = 0x11
+/*
+ 浮点字面量可以是十进制（没有前缀）或者是十六进制（前缀是 0x ）。小数点两边必须有至少一个十进制数字（或者是十六进制的数字）。十进制的浮点字面量还有一个可选的指数，用大写或小写的 e 表示；十六进制的浮点字面量必须有指数，用大写或小写的 p 来表示。
+ */
+1.25e2  //10进制 1.25 *10的平方
+1.25e-2
+0xFp-2
+0xFp2
+let hexadecimalDouble = 0xC.3p0
+
+
 
 
 
